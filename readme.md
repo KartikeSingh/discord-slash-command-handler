5e994d376c0ca95a93a3a90bcac2df0d18790f48
shisui <gwk24yt@gmail.com>
2021-07-11 18:25:36 +0530

    all code ig

---
+++ readme.md
@@ -0,0 +1,221 @@
+# Installations
+
+```
+npm i discord-slash-command-handler
+```
+
+# Basic handler example
+
+```js
+const client = new Discord.client(options);
+const Handler = require('discord-slash-command-handler');
+
+client.on('ready',()=>{
+    const handler = new Handler(client,"src/commands",{guilds:["guild id"]});
+
+    console.log("bot is up");
+});
+
+client.login(token);
+```
+
+# Complex handler example
+
+```js
+const client = new Discord.client(options);
+const Handler = require('discord-slash-command-handler');
+
+client.on('ready',()=>{
+    const handler = new Handler(client,"src/commands",
+    {
+        slashGuilds:["guild id"], // Guild ID(s) where you want to enable slash commands (if slash command isn't global)
+        handleSlash: true, //  If you want automatic slash handler, false for no handler, both for both automaitc and non automatic handler more expalantion is give below
+        handleNormal: false,//  If you want automatic normal handler, false for no handler, both for both automaitc and non automatic handler more expalantion is give below
+        prefix: "k!", // Bot's prefix
+        timeouts: true, // If you want to add timeouts in commands
+        handleTimeout: true, // if you want us to handle timeouts, false if not
+        permissionReply: "You don't have enough permissions to use this command", // reply to send when user don't have enough permissions to use the command
+        timeoutMessage: "You are on a timeout", // reply to send when user is on a timeout
+        errorReply: "Unable to run this command due to errors", // reply to send when there is an error in command
+    });
+
+    console.log("bot is up");
+});
+
+client.login(token);
+```
+
+# Custom Command Handler ( Slash/Normal )
+
+```js
+...
+bot.on('ready',()=>{
+    ...
+
+    // Custom normal command handler
+    handler.on('normalCommand',(command,command_data)=>{
+        // handle the command
+        // command is your normal command object , for command_data go down below to data types
+    })
+
+     
+    handler.on('slashCommand',(command,command_data)=>{
+        // handle the command
+        // command is your normal command object , for command_data go down below to data types
+    })
+    ...
+})
+...
+```
+
+# All available events
+
+```js
+handler.on('commandsCreated',(commands,commandAliases)=>{
+    // this event is invoked when commands are added
+    /**
+      * commands : the collection of all the bot commands
+      * commandAliases : the collection of all the bot command's aliases
+      */
+});
+
+handler.on('slashCommand',(commands,command_data)=>{
+    // this event is invoked when a user used a slash command
+    /**
+      * commands : the command used
+      * command_data : the command data ( for more info read data types at bottom )
+      */
+});
+
+handler.on('normalCommand',(commands,command_data)=>{
+    // this event is invoked when a user used a normal command
+    /**
+      * commands : the command used
+      * command_data : the command data ( for more info read data types at bottom )
+      */
+});
+
+handler.on('lessArguments',(commands,message)=>{
+    // This event is invoked when user don't provides enough arguments in a command
+    /**
+      * commands : the command used
+      * message : the Discord message object
+      */
+});
+
+handler.on('noPermission',(commands,message)=>{
+    // This event is invoked when user don't have enough permissions to use a command
+    /**
+      * commands : the command used
+      * message : the Discord message object
+      */
+});
+
+handler.on('timeout',(commands,message)=>{
+    // This event is invoked when user is on a timeout to use a command
+    /**
+      * commands : the command used
+      * message : the Discord message object
+      */
+});
+
+handler.on('exception',(commands,message,error)=>{
+    // This event is invoked when an unknown error occurs while running a command
+    /**
+      * commands : the command used
+      * message : the Discord message object
+      * error : the error
+      */
+});
+```
+
+# How to define command
+
+```js
+file name : help.js
+
+module.exports = {
+    name:"help", // Name of the command
+
+    description:"Get some help", // Description of the command
+    
+    aliases:["gethelp"], // The aliases for command ( don't works for slash command )
+    
+    category:"general", // the category of command
+    
+    slash: "both", // true => if only slash, false => if only normal, "both" => both slash and normal
+    
+    global:false, // false => work in all guilds provided in options, true => works globally
+    
+    timeout:10000 | '10s', // the timeout on the command
+    
+    args:"< command category > [ command name ]", // Command arguments, <> for required arguments, [] for optional arguments ( please provide required arguments before optional arguments )
+
+    // Arguments for slash commands
+
+    // first method
+    args:"< command category > [ command name ]", // Command arguments, <> for required arguments, [] for optional arguments ( please provide required arguments before optional arguments )
+
+    argsType:"String | String", // OPTIONAL, if you want to specify the argument type
+    // Available Types : String, Integer, Boolean, Channel, User, Role
+    // also Sub_command, Sub_command_group but these aren't tested yet
+
+    argsDescription:"The command category | the command name", // OPTIONAL, if you wanna add a cute little description for arguments
+
+    // Second method
+    // All properties are required, if not provided than you will get an error
+    options:[
+        {
+            name:"name of argument",
+            description:"description of the argument",
+            require:true/false,
+            type:"string"
+        }
+    ],
+
+    // OPTIONAL
+    error:async (errorType, command, message, error)=>{
+        // If you want custom error handler for each command 
+        /*
+         * errorType : errorType ( check in data types at bottom for more info )
+         * command : the command
+         * message : the message object
+         * error : only in exceptions, the error message 
+         */
+    }
+
+    // Required
+    run: async (command_data) => {
+        // you command code
+        // you can replace run to execute too.
+    }
+}
+```
+
+# Date Types
+
+```js
+command_data = {
+    client, // your discord client instance
+    guild, // the guild in which command was used
+    channel, // the channel in which command was used
+    interaction, // interaction if it is an slash command
+    args, // the array of arguments
+    member, // the guild member object
+    message, // the message object if normal command, in slash command it have less attributes ( to check its attribute read slash_message )
+}
+
+slash_message = {
+    member, // the guild member object
+    author, // the user 
+    client, // the instance of your client
+    guild, // the guild where command was used
+    channel, // the channel where command was used
+    interaction, // the ineraction if it is an slash command
+    content, // the message contnet
+}
+
+errorType = "noPermission" | "exception" | "lessArguments" | "timeout";
+```
+
+# Report Problems at
---
+++ src/index.js
@@ -0,0 +1,292 @@
+const EventEmitter = require('./class/eventEmitter');
+const fs = require('fs');
+const ms = require('ms');
+
+const _options = {
+    permissionReply: "You don't have enough permissions to use this command",
+    timeoutMessage: "You are on a timeout",
+    errorReply: "Unable to run this command due to errors",
+    handleSlash: true,
+    handleNormal: false,
+    slashGuilds: ["", ""],
+    prefix: "",
+    timeouts: true,
+    handleTimeout: true,
+}
+
+const { getOptions, getType } = require('./utility');
+
+class commandHandler extends EventEmitter {
+    /**
+     * Discord slash command handler ( Normal command handler is not supported yet, it will automatically handle slash commands )
+     * @param {Client} client Provide your discord client
+     * @param {String} commandFolder path to the folder which includes your command files
+     * @param {Array<String>} guilds the array which includes the guild where you want to add the slash commands
+     * @param {_options} options the handler's options
+     */
+    constructor(client, commandFolder, options = _options) {
+        super();
+        if (typeof (commandFolder) !== "string") throw new Error("Please provide a correct command folder");
+
+        if (!client) throw new Error("Invalid client was provided");
+
+        this.client = client;
+
+        const { path } = require.main;
+
+        this.commandFolder = `${path}/${commandFolder}`;
+
+        if (fs.existsSync(this.commandFolder) === false) throw new Error("Invalid command folder, please provide an correct folder");
+
+        this.options = options;
+        this.client.commands = new Map();
+        this.client.commandAliases = new Map();
+        this.timeouts = new Map();
+
+        this.setCommands().then(() => {
+            this.emit("commandsCreated", this.client.commands, this.client.commandAliases);
+
+            if (this.options.handleSlash === "both" || this.options.handleSlash === true) this.handleSlashCommands();
+            if (this.options.handleNormal === true || this.options.handleNormal === "both") {
+                if (!this.options.prefix) throw new Error("Please provide a prefix, if you want us to handle Normal Commands");
+                this.handleNormalCommands();
+            }
+        })
+    }
+
+    async setCommands() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const commands = fs.readdirSync(this.commandFolder)?.filter(file => file.endsWith(".js"));
+
+                if (commands?.length < 1) reject("no commands");
+
+                let _commands = 0, _slashCommands = 0, _normalCommands = 0;
+
+                for (let i = 0; i < commands.length; i++) {
+                    const command = require(`${this.commandFolder}/${commands[i]}`);
+
+                    if (!command.name || !command.run) continue;
+
+                    this.client.commands.set(command.name, command);
+                    command.name = command.name.replace(/ /g, "-").toLowerCase();
+                    command.aliases?.forEach((v) => this.commandAliases.set(v, command.name));
+
+                    _commands++;
+
+                    if (command.slash !== true) _normalCommands++;
+
+                    if (command.slash === true || command.slash === "both") {
+
+                        if (!command.description) throw new Error("Description is required in a command\n Description was not found in " + command.name)
+
+                        if ((!command.options || command.options.length === 0) && command.args) {
+                            command.options = getOptions(command.args, command.argsDescription, command.argsType);
+                        } else {
+                            for (let i = 0; i < command.options?.length; i++) {
+                                command.options[i].type = getType(command.options[i].type);
+                            }
+                        }
+
+                        const command_data = {
+                            name: command.name,
+                            description: command.description,
+                            options: command.options || [],
+                        }
+
+                        const app = this.client.api.applications(this.client.user.id);
+
+                        if (this.slashGuilds?.length > 0 && command.global !== true) {
+                            app.guilds(this.slashGuilds);
+                        }
+
+                        app.commands.post({
+                            data: command_data,
+                        })
+
+                        _slashCommands++;
+                    }
+                }
+
+                console.log(`[Discord-Slash-Command-Handler] : Added ${_commands} Commands, out of which ${_slashCommands} are slash commands and ${_normalCommands} are normal commands\n\nGuild Slash commands will start working in ${this.options.slashGuilds.length} minutes or less\nGlobal Slash commands will start working after 1 hour`);
+                resolve("done")
+            } catch (e) {
+                reject(e);
+            }
+        })
+    }
+
+    async handleSlashCommands() {
+
+        this.client.ws.on('INTERACTION_CREATE', async (interaction) => {
+            let command;
+            try {
+
+                command = this.client.commands.get(interaction.data.name);
+
+                if (!command) return;
+
+                if (this.timeouts.get(`${interaction.member.user.id}_${interaction.data.name}`)) return this.replyToInteraction(interaction, this.options.timeoutMessage || options.timeoutMessage);
+
+                const args = [], guild = this.client.guilds.cache.get(interaction.guild_id), channel = this.client.channels.cache.get(interaction.channel_id);
+                interaction.data?.options?.forEach((v) => args.push(v.value))
+
+                const message = {
+                    member: interaction.member,
+                    author: interaction.member.user,
+                    client: this.client,
+                    guild: guild,
+                    channel: channel,
+                    interaction: interaction,
+                    content: `/${interaction.data.name} ${args.join(" ")}`,
+                    member: interaction.member,
+                };
+
+                const command_data = {
+                    client: this.client,
+                    guild: guild,
+                    channel: channel,
+                    interaction: interaction,
+                    args: args,
+                    member: interaction.member,
+                    message: message,
+                }
+
+                let allow = command.permissions ? command.permissions.length === 0 : true;
+
+                command.permissions?.forEach((v) => { if (interaction.member.permissions.has(v)) allow = true });
+
+                if (!allow) {
+                    if (typeof (command.errors) === "function") {
+                        command.errors("noPermissions",command, message);
+                    } else {
+                        await this.replyToInteraction(interaction, this.options.permissionReply || _options.permissionReply);
+                    }
+                    return;
+                }
+
+                await this.replyToInteraction(interaction, "command accepted");
+
+                const timeout = (isNaN(command.timeout) && command.timeout) ? ms(command.timeout || " ") : command.timeout || (isNaN(command.cooldown) && command.cooldown) ? ms(command.cooldown || " ") : command.cooldown;
+
+                if (timeout && this.options.timeouts === true) {
+                    this.timeouts.set(`${interaction.member.user.id}_${interaction.data.name}`);
+                    setTimeout(() => this.timeouts.delete(`${interaction.member.user.id}_${interaction.data.name}`), timeout)
+                }
+
+                if (this.options.handleSlash === true) command.run(command_data);
+                else this.emit("slashCommand", command, command_data);
+            } catch (e) {
+                console.log(e)
+                if (typeof (command.errors) === "function") {
+                    command.errors("exception",command, message,e);
+                } else {
+                    await this.replyToInteraction(interaction, this.options.errorReply || _options.errorReply);
+                }
+            }
+        })
+    }
+
+    async handleNormalCommands() {
+        this.client.on('message', async (message) => {
+            let command;
+            try {
+                if (message.author.bot || !message.content.toLowerCase().startsWith(this.options.prefix)) return;
+
+                const args = message.content.slice(this.options.prefix.length).trim().split(/ +/g);
+                let cmd = args.shift().toLowerCase();
+
+                if (cmd.length == 0) return;
+
+                command = this.client.commands.get(cmd) || this.client.commands.get(this.client.commandAliases.get(cmd));
+
+                if (!command || command.slash === true) return;
+
+                if (command.dm !== true && !message.guild) return;
+
+                if (this.timeouts.has(`${message.member.user.id}_${command.name}`)) {
+                    if (this.options.handleTimeout !== false) message.reply(this.options.timeoutMessage || _options.timeoutMessage);
+                    this.emit("timeout", command, message)
+                    if(typeof(command.errors) ==="function")command.errors("timeout",command,message)
+                    return;
+                }
+
+                const reqArgs = getOptions(command, args).filter((v) => v.required === true) || [];
+
+                if (args.length < reqArgs.length) {
+                    if (typeof (command.errors) === "function") {
+                        command.errors("lessArguments", command, message)
+                        this.emit("lessArguments", command, message)
+                    } else {
+                        message.reply(`Invalid Syntax corrected syntax is : \`${this.options.prefix}${command.name} ${command.args}\``);
+                        this.emit("lessArguments", command, message)
+                    }
+                    return;
+                }
+
+                let allow = command.permissions ? command.permissions.length === 0 : true;
+
+                command.permissions?.forEach((v) => { if (message.member.permissions.has(v)) allow = true });
+
+                if (!allow) {
+                    if (typeof (command.errors) === "function") {
+                        command.errors("noPermission", command, message);
+                        this.emit("noPermission", command, message)
+                    } else {
+                        message.reply(this.options.permissionReply || _options.permissionReply);
+                        this.emit("noPermission", command, message)
+                    }
+                    return;
+                }
+
+                const command_data = {
+                    client: this.client,
+                    guild: message.guild,
+                    channel: message.channel,
+                    interaction: undefined,
+                    args: args,
+                    member: message.member,
+                    message: message,
+                }
+
+                let timeout;
+
+                if (command.timeout || command.cooldown) {
+                    if (typeof (command.timeout || command.cooldown) === "string") timeout = ms(command.timeout || command.cooldown)
+                    else timeout = command.timeout || command.cooldown;
+                }
+
+                console.log(timeout)
+                if (timeout && this.options.timeouts === true) {
+                    this.timeouts.set(`${message.member.user.id}_${command.name}`);
+                    setTimeout(() => this.timeouts.delete(`${message.member.user.id}_${command.name}`), timeout)
+                }
+
+                if (this.options.handleNormal === true) command.run(command_data);
+                else this.emit("normalCommand", command, command_data);
+            } catch (e) {
+                console.log(e)
+                this.emit("exception", command, message, e);
+                if (typeof (command.errors) === "function") command.errors("exception", command, message, e);
+            }
+        })
+
+    }
+    async replyToInteraction(interaction, content) {
+        try {
+            this.client.api.interactions(interaction.id, interaction.token).callback.post({
+                data: {
+                    type: 4,
+                    data: {
+                        content: content,
+                        flags: 64
+                    }
+                }
+            })
+        } catch (e) {
+            console.log(`[Discord-Slash-Command-Handler] : ${e}`)
+        }
+    }
+}
+
+module.exports = commandHandler;
