6eca3431d09f571910a7706de54ec198af0db75b
KartikeSingh <gwk24yt@gmail.com>
2022-05-18 12:04:59 +0530

    Case Error

--- source/index.ts
+++ source/index.ts
@@ -1,4 +1,4 @@
-export * as handler from "./classes/handler"
+export * as Handler from "./classes/handler"
 export * as TimeoutModel from "./models/timeout"
 export * as Client from "./classes/Client"
 export * as Options from "./classes/options"
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Options = exports.Client = exports.TimeoutModel = exports.handler = void 0;
-exports.handler = require("./classes/handler");
+exports.Options = exports.Client = exports.TimeoutModel = exports.Handler = void 0;
+exports.Handler = require("./classes/handler");
 exports.TimeoutModel = require("./models/timeout");
 exports.Client = require("./classes/Client");
 exports.Options = require("./classes/options");
